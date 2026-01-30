8920d6563fa6a4260509602bd265a769089cd7da
d-002 <minecraftenjoy@outlook.fr>
2026-01-30 18:48:51 +0100

    client: added portable makefile rule

--- shell_server/client/src/main.c
+++ shell_server/client/src/main.c
@@ -13,26 +13,6 @@ int main(void)
     struct string content = { .data = "hello world", .length = 12 };
     ssize_t count = sock_request(sock, "GET", "/", content);
 
-    char buf[64];
-    size_t length = 0;
-    while (1)
-    {
-        ssize_t count = recv(sock->fd, buf, 63, 0);
-        if (count < 0)
-        {
-            printf("error in recv\n");
-            break;
-        }
-        if (count == 0)
-        {
-            break;
-        }
-
-        length += count;
-        buf[count] = '\0';
-        printf("%s", buf);
-    }
-
     sock_destroy(sock);
     return count < 0;
 }
